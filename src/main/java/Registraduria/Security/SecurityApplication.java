90ef0bb5e0bdb664c5632faed05af4df242ecbb8
fdoduran89 <fdoduran1989@gmail.com>
2022-11-22 21:09:34 -0500

    arreglo securityApplication

--- src/main/java/Registraduria/Security/SecurityApplication.java
+++ src/main/java/Registraduria/Security/SecurityApplication.java
@@ -1,10 +1,17 @@
 package Registraduria.Security;
 
+import Registraduria.Security.models.User;
+import Registraduria.Security.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SecurityApplication {
@@ -17,5 +24,22 @@ public class SecurityApplication {
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+
+    private void withMongoAndQuery(UserRepository repository, MongoTemplate mongoTemplate, User user, String email) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").is(email));
+
+        List<User> users = mongoTemplate.find(query, User.class);
+
+        if (users.size() > 1) {
+            throw new IllegalStateException("Se encontraron varios usuarios con el email " + email);
+        }
+        if (users.isEmpty()) {
+            repository.insert(user);
+        } else {
+            System.out.println(user + " ya existe");
+        }
+    }
 
 }
